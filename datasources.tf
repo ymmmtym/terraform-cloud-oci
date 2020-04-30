afd3b55c36b7acfd67dce2d3126e67ea962123ed
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 13:41:32 +0900

    check datasources

--- datasources.tf
+++ datasources.tf
@@ -2,6 +2,14 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.COMPARTMENT_OCID
 }
 
+data "oci_core_instance" "ubuntu01" {
+  instance_id = "ocid1.instance.oc1.ap-tokyo-1.anxhiljrhki6czacqpkyum7snuooahii4mcx7hvznuynnuo6fgwnw6z7dnsa"
+}
+
 output "ADs" {
   value = lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")
+}
+
+output "instance" {
+  value = data.oci_core_instance.ubuntu01
 }
