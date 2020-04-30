effcf09729272d7392a3076e7110e65cd0d77019
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 16:44:43 +0900

    comment in output

--- compute.tf
+++ compute.tf
@@ -61,9 +61,9 @@ resource "oci_core_instance" "ubuntu02" {
 # }
 
 # output ip addresses
-# output "ubuntu01" {
-#   value = lookup(oci_core_instance.ubuntu01[0], "public_ip")
-# }
-# output "ubuntu02" {
-#   value = lookup(oci_core_instance.ubuntu02[0], "public_ip")
-# }
+output "ubuntu01" {
+  value = lookup(oci_core_instance.ubuntu01[0], "public_ip")
+}
+output "ubuntu02" {
+  value = lookup(oci_core_instance.ubuntu02[0], "public_ip")
+}
