401f53edcb0dfe67383f226c3ab1f4f87966155c
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 15:30:14 +0900

    modify data sources

--- datasources.tf
+++ datasources.tf
@@ -2,14 +2,14 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.COMPARTMENT_OCID
 }
 
-data "oci_core_instance" "ubuntu01" {
-  instance_id = "ocid1.instance.oc1.ap-tokyo-1.anxhiljrhki6czacqpkyum7snuooahii4mcx7hvznuynnuo6fgwnw6z7dnsa"
+data "oci_core_public_ip" "public_ip01" {
+    ip_address = "132.145.118.149"
 }
 
-output "ADs" {
-  value = lookup(data.oci_identity_availability_domains.ads.availability_domains[0],"name")
-}
+# output "ubuntu01" {
+#   value = oci_core_instance.ubuntu01
+# }
 
-output "instance" {
-  value = data.oci_core_instance.ubuntu01
+output "ubuntu02" {
+  value = oci_core_instance.ubuntu02
 }
