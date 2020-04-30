84e67119a3949fcd637aa4909b5a5eee3a1a3c0d
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 10:09:35 +0900

    modify syntax

--- provider.tf
+++ provider.tf
@@ -6,15 +6,15 @@ variable "PRIVATE_KEY_PASSWORD" {}
 variable "REGION" {
   default = "ap-tokyo-1"
 }
-variable "COMPARTMENT_OCID" {
-  default = "${var.TENANCY_OCID}"
-}
+# variable "COMPARTMENT_OCID" {
+#   default = "${var.TENANCY_OCID}"
+# }
 
 provider "oci" {
-  tenancy_ocid = var.TENANCY_OCID
-  user_ocid = var.USER_OCID
-  fingerprint = var.FINGERPRINT
-  private_key = var.PRIVATE_KEY
-  private_key_password = var.PRIVATE_KEY_PASSWORD
-  region = var.REGION
+  tenancy_ocid = "${var.TENANCY_OCID}"
+  user_ocid = "${var.USER_OCID}"
+  fingerprint = "${var.FINGERPRINT}"
+  private_key = "${var.PRIVATE_KEY}"
+  private_key_password = "${var.PRIVATE_KEY_PASSWORD}"
+  region = "${var.REGION}"
 }
