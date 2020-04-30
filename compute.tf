c1d5be82bb41472861c12cf5b1ef05e24c22a507
ymmmtym <h.yukihisa.378@gmail.com>
2020-04-30 14:37:13 +0900

    add compute.tf

--- compute.tf
+++ compute.tf
@@ -1,18 +1,22 @@
-# resource "oci_core_instance" "ubuntu03" {
-#     count = "1"
-#     availability_domain = oci_core_subnet.subnet01.availability_domain
-#     compartment_id = var.COMPARTMENT_OCID
-#     shape = "VM.Standard2.1.Micro"
-#     display_name = "ubuntu03"
-#     create_vnic_details {
-#         subnet_id = oci_core_subnet.subnet01.id
-#     }
-#     source_details {
-#         source_id = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaat4kfe427svas2tocgp3sz6py6hr3od7tgsohuw43lm3fl3gcioiq"
-#         source_type = "image"
-#     }
-#     metadata {
-#         ssh_authorized_keys = var.SSH_PUBLIC_KEY
-#     }
-#     # defined_tags = map(var.defined_tag,  var.defined_tag_value)
-# }
+resource "oci_core_instance" "ubuntu02" {
+    count = "1"
+    availability_domain = oci_core_subnet.subnet01.availability_domain
+    compartment_id = var.COMPARTMENT_OCID
+    shape = "VM.Standard2.1.Micro"
+    display_name = "ubuntu02"
+    create_vnic_details {
+        subnet_id = oci_core_subnet.subnet01.id
+        private_ip = "192.168.0.3"
+        display_name     = "vnic01"
+        assign_public_ip = true
+        hostname_label   = "freeinstance0"
+    }
+    source_details {
+        source_id = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaat4kfe427svas2tocgp3sz6py6hr3od7tgsohuw43lm3fl3gcioiq"
+        source_type = "image"
+    }
+    metadata = {
+        ssh_authorized_keys = var.SSH_PUBLIC_KEY
+    }
+    # defined_tags = map(var.defined_tag,  var.defined_tag_value)
+}
