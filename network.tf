5fb6f6e06d7aeb25b605a0b1f35e2b063b6b08a9
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-01-24 13:51:34 +0900

    delete ipaddr

--- network.tf
+++ network.tf
@@ -78,8 +78,6 @@ resource "oci_load_balancer_load_balancer" "lb01" {
     compartment_id = var.COMPARTMENT_OCID
     display_name = "lb01"
     shape = "10Mbps-Micro"
-    ip_addresses = data.oci_core_public_ip.public_ip01.ip_address
-    is_private = false
     subnet_ids = [
       oci_core_subnet.subnet01.id
     ]
