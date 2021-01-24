a79a8d53cc368ccbae466cbb51c0fd4e6ad3187e
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-01-24 13:31:09 +0900

    fix subnet ids

--- network.tf
+++ network.tf
@@ -75,9 +75,10 @@ resource "oci_core_subnet" "subnet01" {
 }
 
 resource "oci_load_balancer_load_balancer" "lb01" {
-    #Required
     compartment_id = var.COMPARTMENT_OCID
     display_name = "lb01"
     shape = "10Mbps-Micro"
-    subnet_ids = oci_core_subnet.subnet01.id
+    subnet_ids = [ 
+      oci_core_subnet.subnet01.id
+    ]
 }
