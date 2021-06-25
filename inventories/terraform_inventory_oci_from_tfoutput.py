d8a35e9a2aa9ff43b3eab10ee1be805a7a00a29f
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-06-25 15:31:15 +0900

    modify dynamic inventory

---
+++ inventories/terraform_inventory_oci_from_tfoutput.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
+"""Dynamic Inventory from terraform.output for OCI."""
+
+import json
+
+
+TERRAFORM_TFOUTPUT_PATH = './terraform.tfoutput'
+INVENTORY = {'all': {'hosts': []}, '_meta': {'hostvars': {}}}
+TFOUTPUT = json.load(open(TERRAFORM_TFOUTPUT_PATH, 'r'))
+
+for key,value in TFOUTPUT.items():
+        host = key
+        INVENTORY['all']['hosts'].append(host)
+        INVENTORY['_meta']['hostvars'][host] = {}
+        INVENTORY['_meta']['hostvars'][host]['ansible_host'] \
+            = value['value'][0]
+
+
+print(json.dumps(INVENTORY, indent=4))
