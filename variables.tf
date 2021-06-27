f9c96ed0696a6e030b412b66456c9e94f0cfec3c
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-06-27 15:27:38 +0900

    add volume

--- variables.tf
+++ variables.tf
@@ -6,6 +6,9 @@ variable "PRIVATE_KEY_PASSWORD" {}
 variable "REGION" {
   default = "ap-tokyo-1"
 }
+variable "AVAILABILITY_ZONE" {
+  default = "ZMzB:AP-TOKYO-1-AD-1"
+}
 variable "COMPARTMENT_OCID" {}
 variable "SSH_PUBLIC_KEY" {}
 variable "INSTANCE_SHAPE" {
@@ -13,4 +16,7 @@ variable "INSTANCE_SHAPE" {
 }
 variable "INSTANCE_SOURCE_OCID" {
   default = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaat4kfe427svas2tocgp3sz6py6hr3od7tgsohuw43lm3fl3gcioiq"
+}
+variable "VOLUME_ATTACHMENT_ATTACHMENT_TYPE" {
+  default = "iscsi"
 }
