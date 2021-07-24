4b42a7a8beb49c9c867a217d0e626fc4f1229910
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-07-24 21:18:13 +0900

    fix dependance in action

--- terraform/rke.tf
+++ terraform/rke.tf
@@ -1,30 +1,30 @@
 resource "rke_cluster" "cluster" {
   nodes {
-    address          = oci_core_instance.ubuntu01.*.public_ip
-    internal_address = oci_core_instance.ubuntu01.*.private_ip
+    address          = oci_core_instance.ubuntu01[0].public_ip
+    internal_address = oci_core_instance.ubuntu01[0].private_ip
     user             = "ubuntu"
-    ssh_key          = file("~/.ssh/id_rsa")
+    ssh_key          = var.PRIVATE_KEY
     role             = ["controlplane", "worker", "etcd"]
   }
   nodes {
-    address          = oci_core_instance.ubuntu02.*.public_ip
-    internal_address = oci_core_instance.ubuntu02.*.private_ip
+    address          = oci_core_instance.ubuntu02[0].public_ip
+    internal_address = oci_core_instance.ubuntu02[0].private_ip
     user             = "ubuntu"
-    ssh_key          = file("~/.ssh/id_rsa")
+    ssh_key          = var.PRIVATE_KEY
     role             = ["worker"]
   }
   nodes {
-    address          = oci_core_instance.ubuntu03.*.public_ip
-    internal_address = oci_core_instance.ubuntu03.*.private_ip
+    address          = oci_core_instance.ubuntu03[0].public_ip
+    internal_address = oci_core_instance.ubuntu03[0].private_ip
     user             = "ubuntu"
-    ssh_key          = file("~/.ssh/id_rsa")
+    ssh_key          = var.PRIVATE_KEY
     role             = ["worker"]
   }
   nodes {
-    address          = oci_core_instance.ubuntu04.*.public_ip
-    internal_address = oci_core_instance.ubuntu04.*.private_ip
+    address          = oci_core_instance.ubuntu04[0].public_ip
+    internal_address = oci_core_instance.ubuntu04[0].private_ip
     user             = "ubuntu"
-    ssh_key          = file("~/.ssh/id_rsa")
+    ssh_key          = var.PRIVATE_KEY
     role             = ["worker"]
   }
   network {
