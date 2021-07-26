9031b3347e709b73c6f5f4075da142f4f9e0f947
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-07-26 10:46:49 +0900

    refactor oracle linux

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -19,12 +19,12 @@ resource "oci_core_instance" "ubuntu" {
   }
 }
 
-resource "oci_core_instance" "oracle01" {
+resource "oci_core_instance" "oracle_linux" {
   count               = "1"
   availability_domain = oci_core_subnet.subnet01.availability_domain
   compartment_id      = var.COMPARTMENT_OCID
   shape               = "VM.Standard.A1.Flex"
-  display_name        = "oracle01"
+  display_name        = "oracle0${count.index + 1}"
 
   shape_config {
     ocpus         = 1
@@ -33,9 +33,9 @@ resource "oci_core_instance" "oracle01" {
 
   create_vnic_details {
     subnet_id        = oci_core_subnet.subnet01.id
-    display_name     = "vnic01"
+    display_name     = "vnic0${count.index + 1}"
     assign_public_ip = false
-    hostname_label   = "oracle01"
+    hostname_label   = "oracle0${count.index + 1}"
   }
   source_details {
     source_id   = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaakjxlsfb6ltwp7lqhr4owgvwvwtgskjfcwoium6pr35lp2hw4guvq"
