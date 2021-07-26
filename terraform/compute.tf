5329a0413b2678a4d03933dd1f3bbf7712fbc566
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-07-26 09:11:06 +0900

    add instance of VM.Standard.A1.Flex

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -41,6 +41,27 @@ resource "oci_core_instance" "ubuntu02" {
   }
 }
 
+resource "oci_core_instance" "oracle01" {
+  count               = "1"
+  availability_domain = oci_core_subnet.subnet01.availability_domain
+  compartment_id      = var.COMPARTMENT_OCID
+  shape               = "VM.Standard.A1.Flex"
+  display_name        = "oracle01"
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.subnet01.id
+    display_name     = "vnic01"
+    assign_public_ip = false
+    hostname_label   = "oracle01"
+  }
+  source_details {
+    source_id   = "ocid1.instance.oc1.ap-tokyo-1.anxhiljrhki6czac4lecpd2n47n2i5vvby5d2zd23oln3qchzqt5pewbiatq"
+    source_type = "image"
+  }
+  metadata = {
+    ssh_authorized_keys = var.SSH_PUBLIC_KEY
+  }
+}
+
 # output ip addresses
 output "ubuntu01" {
   value = oci_core_instance.ubuntu01.*.public_ip
